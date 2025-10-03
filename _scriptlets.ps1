870f2e82aba5a6c7b368a4d9a76b5c1539a59e9c
Roeland Cerfonteijn <NL12143@hotmail.com>
2025-10-03 14:49:02 +0200

    Prettify

--- _scriptlets.ps1
+++ _scriptlets.ps1
@@ -2,3 +2,8 @@
 
 pac solution unpack --zipfile "C:\TEMP\agent-ai-teams-http\Get Answer from OpenAI MS Teams.zip" --folder "C:\TEMP\agent-ai-teams-http\GetAnswerfromAzureOpenAI-MSTeams"
 
+
+git add definition.json
+git commit -m "Prettify JSON for readability"
+git push
+
